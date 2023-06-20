96ccfa7d1a49c22d8f2a8052c6362ce8fb341fd9
silence <abaird0893@gmail.com>
2023-06-20 18:32:38 +0100

    Added ability to append new bookmarks and auto update data map and localstorage

---
+++ frontend/src/components/bookmarkMap.js
@@ -0,0 +1,56 @@
+import { randomString } from "../functions/randomString";
+import Plus from "../resources/plus.svg";
+
+export default function BookmarkMap(props) {
+  const header = props.bookmarks;
+  const iconID = randomString(2);
+
+  function iconToggler(id) {
+    const x = document.getElementById(id);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function openModal() {
+    document.getElementById(props.modalID).showModal();
+    props.modalSection(props.index);
+  }
+     
+  return (
+    <>
+      <div className="headerSubsection">
+        <div key={header[0]}>
+          <div
+            className="headerBox"
+            onMouseEnter={() => iconToggler(iconID)}
+            onMouseLeave={() => iconToggler(iconID)}
+          >
+            <p className="linkHeader">{header[0]}</p>
+            <img
+              src={Plus}
+              alt="plus symbol"
+              className="icon"
+              id={iconID}
+              onClick={() => openModal()}
+              style={{ display: "none" }}
+              title="add new bookmark"
+              width="24px"
+            />
+          </div>
+          {header[1].map((entry) => {
+            return (
+              <a key={entry.name} href={entry.url} className="link">
+                <p style={{ padding: "10px" }} className="link">
+                  {entry.name}
+                </p>
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ frontend/src/components/bookmarksPage.js
@@ -0,0 +1,117 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { randomString } from "../functions/randomString";
+
+import BookmarkMap from "./bookmarkMap";
+
+export default function Bookmarks(props) {
+  const navigate = useNavigate();
+  // modal ///////////////////////////////////////////////////////////////////////////
+  const modalID = "addNewBookmarkModal";
+  const [modalSection, setModalSection] = useState("");
+  // data ///////////////////////////////////////////////////////////////////////////
+  const display = Object.entries(props.bookmarks);
+  const [dataMap, updateDataMap] = useState(dataMapper());
+  const [validData, setValidData] = useState("");
+
+  useEffect(() => {
+    if (typeof display !== "object") {
+      setValidData(false);
+    } else {
+      setValidData(true);
+    }
+  }, [display]);
+
+  // functions ////////////////////////////////////////////////////////////////////////
+  function dataMapper() {
+    return display.map((header, index) => {
+      return (
+        <BookmarkMap
+          key={randomString(4)}
+          bookmarks={header}
+          index={index}
+          modalID={modalID}
+          modalSection={(modalSection) => setModalSection(modalSection)}
+        />
+      );
+    });
+  }
+  function submitNewBookmark(e) {
+    e.preventDefault();
+    const newBookmark = {
+      name: e.target.name.value,
+      url: e.target.url.value,
+    };
+    display[modalSection][1].push(newBookmark);
+    updateDataMap(dataMapper());
+    props.updateBookmarks(Object.fromEntries(display));
+    document.getElementById("newBookmarkForm").reset();
+    document.getElementById(modalID).close();
+  }
+
+  function manualBookmarkDelete() {
+    document.cookie = "bookmarksStorage=removed; sameSite=Strict";
+    props.updateBookmarks("");
+  }
+
+  function jsonDownloader() {
+    const data = JSON.stringify(props.bookmarks);
+    props.daemon.downloader(data, "links", "json");
+  }
+  return validData === true ? (
+    <>
+      {/*/////////////// MODAL /////////////////////////////////////////////////////*/}
+      <dialog closed="true" id={modalID} className="dialog">
+        <form
+          method="dialog"
+          id="newBookmarkForm"
+          className="formInputs"
+          onSubmit={(e) => submitNewBookmark(e)}
+        >
+          <label>name:</label>
+          <input name="name" type="text" required />
+          <label>url:</label>
+          <input name="url" type="text" required />
+          <button type="submit" className="border">
+            add new bookmark
+          </button>
+        </form>
+      </dialog>
+      {/*/////////////// MAP /////////////////////////////////////////////////////*/}
+      <div className="textCenter">
+        <div className="linksBox">{dataMap}</div>
+      </div>
+      <div id="dataInteractionButtons" className="dataButtons">
+        {/* TODO
+      enable encryption of current bookmarsk, 
+      autofill bookmarks string into form with ability to select algorithm and input secret key
+      */}
+        <button className="lowerButton" onClick={() => navigate("/encrypt")}>
+          encrypt current bookmarks
+        </button>
+        <button className="lowerButton" onClick={() => navigate("/encrypt")}>
+          encrypt new file
+        </button>
+        <button className="lowerButton" onClick={() => manualBookmarkDelete()}>
+          remove data from memory
+        </button>
+        <button className="lowerButton" onClick={() => jsonDownloader()}>
+          download unencrypted json
+        </button>
+      </div>
+    </>
+  ) : (
+    <>
+      <div className="textCenter">
+        <p>
+          Invalid data. Data does not appear to be valid JSON format. Or, if
+          using AES encryption, you may have typed in the wrong key.
+        </p>
+        <button className="lowerButton" onClick={() => manualBookmarkDelete()}>
+          reupload data
+        </button>
+      </div>
+    </>
+  );
+}
